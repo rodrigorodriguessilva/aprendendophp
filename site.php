c07cc66edb7fb6f57e0ab5b69a362341a745dd8b
rodrigo <rodrigo2rodrigues@outlook.com>
2024-04-30 17:13:57 -0300

    upload

---
+++ site.php
@@ -0,0 +1,12 @@
+<?php
+	echo '<h1>Ola mundo</h1>'; 	
+	$nome = $_POST['nome'];
+	$endereco = $_POST['endereco'];
+	echo $nome;	
+	echo '<p>Voce mora na rua</p>';
+	echo $endereco;
+	echo '<br />';
+	$teste = $_POST['teste'];
+	echo $teste;
+
+?>
