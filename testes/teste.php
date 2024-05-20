cbf198b349c4424a7d032285751b5d7e2eb7a904
rodrigo <rodrigo2rodrigues@outlook.com>
2024-05-20 15:05:50 -0300

    aula 2005

---
+++ testes/teste.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    /*mySqli
+PDO
+
+mysql()  <- antigo e com falhas
+
+
+
+<?php =$SERVER["PHP_SELF"]?>
+
+
+
+<?php 
+echo "ola mundo";
+?>
+
+
+$obj_mysqli = new mysqli(
+"servidor",
+"usuario",
+"senha",
+"database"
+);
+
+$maior = false;
+if ($maior){ // true or false
+	echo "bloco verdadeiro";
+}else{
+	echo "bloco falso";
+}
+
+
+
+
+soma(10,20);
+
+
+public void function soma(a,b){
+	resultado = a+b //30
+	echo resultado;
+}
+
+
+
+arrow function
+
+
+OU   ||   OR
+E    &&   AND
+NÃO  !     NOT
+*/
+        
+        
+
+        $teste = soma(10,9);
+        $teste = $teste/2;
+        echo $teste;
+        
+         function soma($nota1,$nota2){
+            $resultado = $nota1+$nota2;
+            return $resultado;
+         }
+    
+    ?>
+
+    
+</body>
+</html>
